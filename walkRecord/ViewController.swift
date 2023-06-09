98f19a6759c02b0c067199e8480e73b538826304
Sooyeon-Kwon <kwonsy159@naver.com>
2023-06-09 20:45:01 +0900

    Initial Commit

---
+++ walkRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walkRecord
+//
+//  Created by 권수연 on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
